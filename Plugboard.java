ab36b7554d7368db6ae31aaaa728c548cf2f7ba8
CaydenD <69131993+CaydenD@users.noreply.github.com>
2020-08-03 14:35:32 -0400

    Update Plugboard.java

--- Plugboard.java
+++ Plugboard.java
@@ -1,16 +1,10 @@
-// Remember, you can't change the interface, nor add import statements
+
 
 public class Plugboard {
-    // decide on your instance variables to represent the plugboard
-    
-    // NOTE:  you need to be able to map and reverse-map
-    //        (remember the direction of flow when discussed in class)
 
     //array of swaps
     private Character[] swaps;
     
-    // swaps is a character array with upto 10 pairs of characters to
-    // swap (eg. ['a', 'j', 'q', 'z', 'b', 'w'])
     public Plugboard(Character[] swaps) {  // construct a Plugboard
 
         this.swaps = swaps;
@@ -26,7 +20,6 @@ public class Plugboard {
     }
 
     // encode one character through the plugboard
-    // (you may assume that ch is a valid character in range ['a', 'z']
     public char encode(char ch) {
 
         // if the given ch is in the swaps object array it encodes it to the given char
@@ -49,8 +42,6 @@ public class Plugboard {
 	return ch;   // CHANGE THIS, JUST HERE IN ORDER TO COMPILE
     }
 
-    // decode one character through the plugboard
-    // (you may assume that ch is a valid character in range ['a', 'z']
     public char decode(char ch) {
 
         // if the given ch is in the swaps object array it decodes it to the given char
